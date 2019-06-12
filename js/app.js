440dca57d59873f4804672fec9966293d8eba461
EdinK1 <edinkaymakchi@gmail.com>
2019-06-12 03:08:10 -0700

    complete most of the website

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // I know that the code could be better.
 // If you have some tips or improvement, please let me know.
 
+// Parallax effect
+
 $('.img-parallax').each(function(){
     var img = $(this);
     var imgParent = $(this).parent();
@@ -12,16 +14,11 @@ $('.img-parallax').each(function(){
       var parentH = imgParent.innerHeight();
   
   
-      // The next pixel to show on screen      
       var winBottom = winY + winH;
   
-      // If block is shown on screen
       if (winBottom > imgY && winY < imgY + parentH) {
-        // Number of pixels shown after block appear
         var imgBottom = ((winBottom - imgY) * speed);
-        // Max number of pixels until block disappear
         var imgTop = winH + parentH;
-        // Porcentage between start showing until disappearing
         var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
       }
       img.css({
@@ -37,3 +34,24 @@ $('.img-parallax').each(function(){
       }
     });
   });
+
+  // Smooth Scrolling
+  $("a").on("click", function(event) {
+    if (this.hash != "") {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800
+      );
+    }
+  });
+
+  $(".project").hover(function () {
+      $(this).children(".project-link-project").show();
+  });
+
